0d88e80e1510aa8f796e11667ea05ce0161a17b3
Praful <prafulrakhade02@gmail.com>
2022-07-07 16:56:07 +0530

    [praful] Added User Registration Fundoonotes program

---
+++ RepositoryLayer/Services/UserRL.cs
@@ -0,0 +1,45 @@
+﻿using DatabaseLayer;
+using Microsoft.Extensions.Configuration;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class UserRL : IUserRL
+    {
+        private readonly string connectionString;
+        public UserRL(IConfiguration configuartion)
+        {
+            connectionString = configuartion.GetConnectionString("Fundoonotes");
+        }
+        public void AddUser(UserModel userModel)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    //Creating a stored Procedure for adding Users into database
+                    SqlCommand com = new SqlCommand("spAddUser", connection);
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.Parameters.AddWithValue("@FirstName", userModel.FirstName);
+                    com.Parameters.AddWithValue("@LastName", userModel.LastName);
+                    com.Parameters.AddWithValue("@Email", userModel.Email);
+                    com.Parameters.AddWithValue("@Password", userModel.Password);
+                    var result = com.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
